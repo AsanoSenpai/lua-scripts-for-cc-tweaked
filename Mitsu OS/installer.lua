62d3b1c6809ec93ea5edeaf5aba8cd5c6e8a5e8d
Asano_Senpai <delonville.theo@outlook.com>
2024-09-25 16:10:55 +0200

    Updated Mitsu OS

--- Mitsu OS/installer.lua
+++ Mitsu OS/installer.lua
@@ -140,6 +140,72 @@ end
 			
 print("Download Finished")
 
+-- download file using wget
+local function downloadFile(url, destination)
+	shell.run("wget", url, destination)
+end
+
+-- download directory and its contents recursively
+local function downloadDirectory(url, destination)
+	shell.run("mkdir", destination)
+	shell.run("cd", destination)
+
+	local listing = http.get(url)
+	local content = listing.readAll()
+	listing.close()
+
+	local files = textutils.unserializeJSONN(content)
+
+	for _, file in ipairs(files) do
+		if file.type == "file" then
+			downloadFile(file.download_url, file.name)
+		elseif file.type == "dir" then
+			downloadDirectory(file.url, file.name)
+		end
+	end
+
+	shell.run("cd", "..")
+end
+
+-- Get repository name from URL
+local function getRepositoryName(url)
+	local _, _, repositoryName = string.find(url, "https://github.com/(.-)/")
+	return repositoryName
+end
+
+-- Delete existing files in the repository
+local function deleteExistingFiles(repositoryName)
+	shell.run("rm", "-rf", repositoryName)
+end
+
+-- start repository download
+print("Welcome to the Mitsu OS Installation Program")
+print("--------------------------------------------")
+
+-- prompt user for confirmation
+print("This program will install Mitsu OS on your computer.")
+print("Note: All existing files will be deleted.")
+print("Do you want to continue? (y/n)")
+local confirm = read()
+
+if confirm == "y" or confirm == "Y" then
+	-- delete existing files
+	local repositoryName = getRepositoryName(repoUrl)
+	deleteExistingFiles(repositoryName)
+
+	-- start download
+	print("Downloadting Mitsu OS...")
+	downloadDirectory(repoUrl, repositoryName)
+	print("Mitsu OS installation completed!")
+
+else
+	print("Mitsu OS installation canceled. Exiting program.")
+end
+
+
+
+
+
 
 -- we wait for an enter, then reboot
 term.clear()
